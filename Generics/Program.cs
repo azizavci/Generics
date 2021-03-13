10a95e056c9043afd21cff62a59cd39aa5be42a2
azizavci.x@gmail.com <azizavci.x@gmail.com>
2021-03-13 14:43:20 +0300

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("aziz");
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.WriteLine(isimler.Length);
+
+
+
+        }
+    }
+}
